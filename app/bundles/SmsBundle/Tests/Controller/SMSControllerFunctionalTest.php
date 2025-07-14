adda7de2df1b080cbb89488c0089f1e945656689
Zdeno Kuzmany <zdeno@kuzmany.biz>
2025-07-14 22:50:25 +0200

    Rename test file to correct case and remove duplicate
    - Move SmsControllerFunctionalTest.php to SMSControllerFunctionalTest.php
    - Remove the original lowercase filename to avoid naming conflicts

--- app/bundles/SmsBundle/Tests/Controller/SMSControllerFunctionalTest.php
+++ app/bundles/SmsBundle/Tests/Controller/SMSControllerFunctionalTest.php
@@ -5,50 +5,91 @@ declare(strict_types=1);
 namespace Mautic\SmsBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
-use Mautic\ProjectBundle\Entity\Project;
 use Mautic\SmsBundle\Entity\Sms;
 use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-final class SMSControllerFunctionalTest extends MauticMysqlTestCase
+class SmsControllerFunctionalTest extends MauticMysqlTestCase
 {
-    protected function setUp(): void
+    public function testSmsListView(): void
     {
-        $this->configParams['site_url'] = 'https://localhost';
-        parent::setUp();
-    }
+        $this->setupTwilio();
 
-    public function testSmsWithProject(): void
-    {
-        $sms = $this->CreateSms();
+        $sms = $this->createSms('ABC', 'content of sms', 'list');
 
-        $project = new Project();
-        $project->setName('Test Project');
-        $this->em->persist($project);
-
+        $this->em->persist($sms);
         $this->em->flush();
-        $this->em->clear();
+        $this->em->detach($sms);
 
-        $crawler = $this->client->request('GET', '/s/sms/edit/'.$sms->getId());
-        $form    = $crawler->selectButton('Save')->form();
-        $form['sms[projects]']->setValue((string) $project->getId());
+        $this->client->request('GET', '/s/sms');
+        $clientResponse  = $this->client->getResponse();
+        $responseContent = $clientResponse->getContent();
+        $this->assertTrue($clientResponse->isOk());
 
-        $this->client->submit($form);
+        $routeAlias = 'sms';
+        $column     = 'stats';
+        $this->assertStringContainsString(
+            'col-'.$routeAlias.'-'.$column,
+            $responseContent,
+            'The return must contain the stats column'
+        );
 
-        $this->assertResponseIsSuccessful();
+        $this->assertStringContainsString(
+            'sms_sent:'.$sms->getId(),
+            $responseContent,
+            'The return must contain sms_sent:1'
+        );
 
-        $savedSms = $this->em->find(Sms::class, $sms->getId());
-        Assert::assertSame($project->getId(), $savedSms->getProjects()->first()->getId());
+        $this->assertStringNotContainsString(
+            'sms_delivered:1'.$sms->getId(),
+            $responseContent,
+            'The return must not contain sms_sent:1'
+        );
+
+        $this->assertStringNotContainsString(
+            'sms_read:1'.$sms->getId(),
+            $responseContent,
+            'The return must not contain sms_read:1'
+        );
+
+        $this->assertStringNotContainsString(
+            'sms_failed:1'.$sms->getId(),
+            $responseContent,
+            'The return must not contain sms_failed:1'
+        );
     }
 
-    private function CreateSms(string $name = 'sms', string $message = 'sms body'): Sms
+    private function createSms(string $name, string $message, string $smsType): Sms
     {
         $sms = new Sms();
         $sms->setName($name);
         $sms->setMessage($message);
-        $sms->setSmsType('template');
+        $sms->setSmsType($smsType);
         $this->em->persist($sms);
-        $this->em->flush();
 
         return $sms;
+    }
+
+    private function setupTwilio(): void
+    {
+        $integration = static::getContainer()->get('mautic.integration.twilio');
+        $crawler     = $this->client->request(Request::METHOD_GET, 's/plugins/config/'.$integration->getName());
+        $response    = $this->client->getResponse();
+
+        Assert::assertSame(Response::HTTP_OK, $response->getStatusCode(), $response->getContent());
+
+        $saveButton = $crawler->selectButton('integration_details[buttons][save]');
+        $form       = $saveButton->form();
+
+        $form['integration_details[apiKeys][username]']->setValue('test_username');
+        $form['integration_details[apiKeys][password]']->setValue('test_password');
+        $form['integration_details[isPublished]']->setValue('1');
+        $form['integration_details[featureSettings][messaging_service_sid]']->setValue('messaging_sid');
+
+        $this->client->submit($form);
+
+        $response = $this->client->getResponse();
+        Assert::assertSame(Response::HTTP_OK, $response->getStatusCode(), $response->getContent());
     }
 }
