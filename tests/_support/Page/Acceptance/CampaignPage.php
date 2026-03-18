90440810802c08f293b11342fb0e5f280732a361
John Linhart <jan@linhart.email>
2026-03-18 08:45:26 +0100

    Removing unused variable

--- tests/_support/Page/Acceptance/CampaignPage.php
+++ tests/_support/Page/Acceptance/CampaignPage.php
@@ -4,9 +4,7 @@ namespace Page\Acceptance;
 
 class CampaignPage
 {
-    public static $URL          = 's/campaigns/view/1';
-    public static $contactsURL  = 's/campaigns/view/1/contact/1';
-
+    public static $URL                  = 's/campaigns/view/1';
     public static $contactsTab          = '//ul[contains(@class, "nav-tabs")]/li/a[@href="#leads-container"]';
     public static $contactsTabContainer = '#leads-container';
 
